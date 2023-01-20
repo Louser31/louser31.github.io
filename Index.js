2c58fcc1fcce715457f6c4d719c447d2da183cc4
Saransh Sharma <31778755+Louser31@users.noreply.github.com>
2023-01-20 18:29:25 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,87 @@
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0 solid #fff}";
+    document.body.appendChild(css);
+
+
+    var slides = document.getElementsByClassName('carousel-item'),
+        addActive = function(slide) {slide.classList.add('active')},
+        removeActive = function(slide) {slide.classList.remove('active')};
+    addActive(slides[0]);
+    
+    setInterval(function (){
+      for (var i = 0; i < slides.length; i++){
+        if (i + 1 == slides.length) {
+          addActive(slides[0]);
+          slides[0].style.zIndex = 100;
+          setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+          break;
+        }
+        if (slides[i].classList.contains('active')) { 
+          slides[i].removeAttribute('style');
+          setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+          addActive(slides[i + 1]);
+          break;
+        }
+      } 
+    }, 3000);
+
+};
+
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+
+
+
+
+};
+
